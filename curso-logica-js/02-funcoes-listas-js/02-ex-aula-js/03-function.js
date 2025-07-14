0e824221d4225737425ffd291144d6e28f1cf3b3
Ingrid Bini <binifidelis@gmail.com>
2025-07-14 11:30:29 -0300

    Function

---
+++ curso-logica-js/02-funcoes-listas-js/02-ex-aula-js/03-function.js
@@ -0,0 +1,11 @@
+//1
+function massaCorporal(altura, peso) {
+    return peso / (altura * altura)  ;
+} 
+let resultadoImc = massaCorporal(1.75, 50);
+console.log(resultadoImc);
+
+//2 
+function valorFatorial() {
+    
+}
