1477c1dd5ddb9bae52bab4a841554bc163e9d8dd
Ingrid Bini <binifideli@gmail.com>
2025-07-14 21:36:26 -0300

    functions

--- curso-logica-js/02-funcoes-listas-js/02-ex-aula-js/03-function.js
+++ curso-logica-js/02-funcoes-listas-js/02-ex-aula-js/03-function.js
@@ -6,6 +6,47 @@ let resultadoImc = massaCorporal(1.75, 50);
 console.log(resultadoImc);
 
 //2 
-function valorFatorial() {
+function valorFatorial(numero) {
     
 }
+
+
+//3 
+function converteDolar(dolar) {
+    let valorDolar = 5.59;
+    let valorReais = dolar * valorDolar;
+    return valorReais
+}
+
+
+//4
+function perimetroArea(altura, largura) {
+    area = altura * largura
+    perimetro = 2 * (altura + largura)
+    return area, perimetro
+
+}
+let resultadoPerimeArea = perimetroArea(15, 5);
+    console.log(`Área ${area} metros quadrados`);
+    console.log(`Perímetro ${perimetro} metros`);
+
+
+//5
+function circuloPerimetro(raio) {
+    area2 = Math.PI * raio * raio;
+    perimetro2 = 2 * Math.PI * raio;
+    return area2, perimetro2
+}
+let resultadoCirculoP = circuloPerimetro(10);
+    console.log(resultadoCirculoP);
+    console.log(`Área do circulo ${area2} metros quadrados`);
+    console.log(`Perímetro do circulo ${perimetro2} metros`);
+
+//6
+function tabuada(numeroDado) {
+    for(let i = 1; i <=10; i++) {
+        let resultado = numeroDado * i;   
+        console.log(`${numeroDado} x ${i} = ${resultado}`)
+    }
+}
+let resultadoTabuada = tabuada(10);
